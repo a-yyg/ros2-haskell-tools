c015ac05b372586f4eb83ce1bfa515a157a78ee3
Yuri Guimaraes <yuri.kgpps@gmail.com>
2024-08-10 12:48:30 +0900

    Change name of executables

--- src/DepCheck.hs
+++ src/DepCheck.hs
@@ -1,8 +1,6 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# OPTIONS_GHC -fno-cse #-}
 
-module Main where
-
 -- import Text.XML.HXT.Parser.XmlParsec (xread)
 
 -- import Control.Applicative
@@ -12,27 +10,31 @@ import Ros2.PackageParser
 import System.Console.CmdArgs
 import Text.Pretty.Simple
 
-data Options = Options
+data DepCheck = DepCheck
   { file :: FilePath
   }
   deriving (Show, Data, Typeable)
 
-options :: Options
-options =
-  Options
+depChkOpt :: DepCheck
+depChkOpt =
+  DepCheck
     { file =
         def
           &= typ "FILE"
           &= opt "package.xml"
           &= help "The package.xml file to parse"
     }
+    &= summary "depcheck v0.1.0.0"
+    &= program "depcheck"
 
 main :: IO ()
 main = do
-  opts <- cmdArgs options
+  opts <- cmdArgs depChkOpt
   content <- readFile $ file opts
-  print content
-  pPrint $ parsePackageXML content
+  -- print content
+  case parsePackageXML content of
+    Left err -> print err
+    Right parsed -> pPrint parsed
 
 -- package <- parsePackageXML content
 -- print package
